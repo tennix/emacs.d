3e52d729fc076466fda7ba2240cc083eb7026deb
tennix <ztennix@gmail.com>
2015-08-12 22:37:26 +0800

    add font settings

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,5 +1,10 @@
 (require-package 'monokai-theme)
 
+;;; set font according to system type
+(when (string-equal system-type "darwin") (set-default-font "monaco-13")
+      (string-equal system-type "gnu/linux") (set-default-font "Fira Mono Medium-12"))
+
+;;; theme: monokai
 (load-theme 'monokai t)
 
 (provide 'init-themes)
