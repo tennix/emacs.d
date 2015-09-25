e2a864e6e3f059e3c4031c4271406912ca08bb3e
tennix <ztennix@gmail.com>
2015-09-25 18:34:31 +0800

    racer become racer-mode

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,18 +1,20 @@
 (require-package 'rust-mode)
+(require-package 'racer)
 (require-package 'flycheck-rust)
 
 (setq racer-rust-src-path "~/Projects/rust/rust/src/")
 (setq racer-cmd "~/Projects/rust/racer/target/release/racer")
-(add-to-list 'load-path "~/Projects/rust/racer/editors/emacs")
-(eval-after-load "rust-mode" '(require 'racer))
 
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
 (add-hook 'rust-mode-hook
 	  '(lambda ()
-	     (racer-activate)
+	     (racer-mode)
 	     (flycheck-mode)
 	     (local-set-key (kbd "M-.") #'racer-find-definition)
 	     (local-set-key (kbd "TAB") #'racer-complete-or-indent)))
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+(setq company-tooltip-align-annotations t)
 
 (provide 'init-rust)
