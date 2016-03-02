8d7e0c750868ea26b6abe261ba3ffcc8d81338cc
tennix <ztennix@gmail.com>
2016-03-02 13:07:06 +0800

    change theme to solarized dark

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,10 +1,12 @@
-(require-package 'monokai-theme)
+;; (require-package 'monokai-theme)
+(require-package 'solarized-theme)
 
 ;;; set font according to system type
 (when (string-equal system-type "darwin") (set-default-font "monaco-13")
       (string-equal system-type "gnu/linux") (set-default-font "Fira Mono Medium-12"))
 
 ;;; theme: monokai
-(load-theme 'monokai t)
+;; (load-theme 'monokai t)
+(load-theme 'solarized-dark t)
 
 (provide 'init-themes)
