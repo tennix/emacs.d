bbb7dcefeb707279942d3053bc497d669188b7c7
tennix <ztennix@gmail.com>
2016-04-09 10:04:50 +0800

    add ocaml merlin mode

--- lisp/init-ocaml.el
+++ lisp/init-ocaml.el
@@ -1,5 +1,11 @@
 (require-package 'tuareg)
+(require-package 'merlin)
 
-
+(add-hook 'tuareg-mode-hook 'merlin-mode t)
+(add-hook 'caml-mode-hook 'merlin-mode t)
+(setq merlin-command 'opam)
+(with-eval-after-load 'company
+					  (add-to-list 'company-backends 'merlin-company-backend))
+(add-hook 'merlin-mode-hook 'company-mode)
 
 (provide 'init-ocaml)
