200208f57502929156ac1fe42d88885fc0e4cc16
tennix <ztennix@gmail.com>
2016-08-17 14:45:38 +0800

    add utop and ocp-indent for ocaml

--- lisp/init-ocaml.el
+++ lisp/init-ocaml.el
@@ -1,11 +1,15 @@
 (require-package 'tuareg)
 (require-package 'merlin)
+(require-package 'utop)
+(require-package 'ocp-indent)
 
 (add-hook 'tuareg-mode-hook 'merlin-mode t)
+(add-hook 'tuareg-mode-hook 'utop-minor-mode)
 (add-hook 'caml-mode-hook 'merlin-mode t)
 (setq merlin-command 'opam)
 (with-eval-after-load 'company
   (add-to-list 'company-backends 'merlin-company-backend))
 (add-hook 'merlin-mode-hook 'company-mode)
 
+(setq utop-command "opam config exec -- utop -emacs")
 (provide 'init-ocaml)
