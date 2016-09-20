60035b9b03894b89d54ef9307427dc52aef32ba4
tennix <ztennix@gmail.com>
2016-09-20 16:22:46 +0800

    fix default font setting

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -2,8 +2,8 @@
 (require-package 'solarized-theme)
 
 ;;; set font according to system type
-(when (string-equal system-type "darwin") (set-default-font "monaco-13")
-      (string-equal system-type "gnu/linux") (set-default-font "Fira Mono Medium-12"))
+(cond ((string-equal system-type "darwin") (set-default-font "monaco-13"))
+      ((string-equal system-type "gnu/linux") (set-default-font "Fira Mono Medium-12")))
 
 ;;; theme: monokai
 ;; (load-theme 'monokai t)
