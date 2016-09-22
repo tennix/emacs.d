18ea420695ed6da877e8bd89f5c4157a353760d4
tennix <ztennix@gmail.com>
2016-09-23 00:53:35 +0800

    add elpy for python; enable flycheck for program-mode

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -4,6 +4,7 @@
 	  flycheck-idle-change-delay 0.8)
 
 (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+(add-hook 'prog-mode-hook 'flycheck-mode)
 
 
 (provide 'init-flycheck)
--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,5 +1,10 @@
 (require-package 'highlight-indentation)
+(require-package 'elpy)
+
+;;; install following packages through pip
+;;; flake8, autopep8, yapf, importmagic, jedi
 
 (add-hook 'python-mode-hook 'highlight-indentation-mode)
+(add-hook 'python-mode-hook 'elpy-mode)
 
 (provide 'init-python)
