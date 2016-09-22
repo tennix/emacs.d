4c3cca080e6666aa3ec3fa5d0f367bde954bf769
tennix <ztennix@gmail.com>
2016-09-23 01:11:26 +0800

    add package.el header; separate customization from init.el to custom.el

--- init.el
+++ init.el
@@ -1,5 +1,14 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
+
 (defconst *is-a-mac* (eq system-type 'darwin))
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 (require 'init-benchmarking)
 (require 'init-elpa)
