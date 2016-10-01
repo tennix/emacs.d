b81e8caedabfe70f7c4b5887098556b8bfd210fa
tennix <ztennix@gmail.com>
2016-10-02 00:15:07 +0800

    disable font scale for org headings

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -5,6 +5,9 @@
 (cond ((string-equal system-type "darwin") (set-default-font "monaco-13"))
       ((string-equal system-type "gnu/linux") (set-default-font "Fira Mono Medium-12")))
 
+(setq solarized-use-variable-pitch nil)
+(setq solarized-scale-org-headlines nil)
+
 ;;; theme: monokai
 ;; (load-theme 'monokai t)
 (if (display-graphic-p)
