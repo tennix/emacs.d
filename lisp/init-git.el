d00d5eb9f63623c80389c29a2f2463703e44d084
tennix <ztennix@gmail.com>
2016-10-01 18:04:43 +0800

    add keybindings to navigate git-gutter hunks

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -2,7 +2,13 @@
 (require-package 'git-gutter)
 
 (global-set-key (kbd "C-x g") 'magit-status)
+
 (global-git-gutter-mode t)
+;;; TODO: git-gutter doesn't support nlinum currently
+(git-gutter:linum-setup)		; this is useless
+;;; jump to previous/next hunk
+(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
 
 (diminish 'git-gutter-mode)
 
