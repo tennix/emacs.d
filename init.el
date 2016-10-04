9e5dc427e092711124377aee202154589929dd59
tennix <ztennix@gmail.com>
2016-10-04 19:37:12 +0800

    remove after-load in exec-path-from-shell

--- init.el
+++ init.el
@@ -159,13 +159,12 @@
 (use-package exec-path-from-shell
   :when (memq window-system '(mac ns))
   :config
-  (after-load 'exec-path-from-shell
-	      (dolist (var '("SSH_AUTH_SOCK"
-			     "SSH_AGENT_PID"
-			     "GPG_AGENT_INFO"
-			     "LANG"
-			     "LC_CTYPE"))
-		(add-to-list 'exec-path-from-shell-variables var)))
+  (dolist (var '("SSH_AUTH_SOCK"
+		 "SSH_AGENT_PID"
+		 "GPG_AGENT_INFO"
+		 "LANG"
+		 "LC_CTYPE"))
+    (add-to-list 'exec-path-from-shell-variables var))
   (exec-path-from-shell-initialize))
 
 
