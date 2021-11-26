fafb5d887961e581434e4aa00b0e18eade1d2f71
tennix <ztennix@gmail.com>
2021-11-27 07:54:50 +0800

    Remove org-roam

--- init.el
+++ init.el
@@ -190,21 +190,6 @@
 ;; org insert code: C-c C-,
 (setq org-log-done 'time)
 (setq org-agenda-files (list "~/my-org-files/"))
-(use-package org-roam
-      :init
-      (setq org-roam-v2-ack t)
-      (setq org-roam-directory (file-truename "~/my-org-files/"))
-      :bind (("C-c n l" . org-roam-buffer-toggle)
-             ("C-c n f" . org-roam-node-find)
-             ("C-c n g" . org-roam-graph)
-             ("C-c n i" . org-roam-node-insert)
-             ("C-c n c" . org-roam-capture)
-             ;; Dailies
-             ("C-c n j" . org-roam-dailies-capture-today))
-      :config
-      (org-roam-setup)
-      ;; If using org-roam-protocol
-      (require 'org-roam-protocol))
 
 (use-package org-download
   :after org
